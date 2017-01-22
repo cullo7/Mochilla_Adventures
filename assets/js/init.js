04c8199ae2b64ade2468deaa9f9de69e652ec63d
cullo7 <culloaiden3@gmail.com>
2017-01-21 23:17:17 -0500

    changed dropdown, added other pages

--- assets/js/init.js
+++ assets/js/init.js
@@ -74,13 +74,3 @@
 	});
 
 })(jQuery);
-
-$(function() {
-    $('a.alt.button').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
